3f5b52f0b2161ea379b43bc88e5a213f7e735939
Julien Kauffmann <julien.kauffmann@freelan.org>
2015-05-14 17:53:33 -0400

    Replaced hooks with tox

--- hooks/install.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set HOOKS_DIR=%~dp0
-
-git config alias.runtests "!python '%HOOKS_DIR%\runtests.py'"
--- hooks/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-HOOKS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-git config alias.runtests "!python '${HOOKS_DIR}/runtests.py'"
