acf49f3200d711d15c2a2392166b4f2ad3fe5930
Julien Kauffmann <julien.kauffmann@freelan.org>
2016-05-25 08:38:27 -0400

    Added Python 3.5 to Travis

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ message.
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: MIT License',
